76fe3047b00a5ad5cb5fc20a90f2e24529da48a3
Diego Cifuentes <diego.o.cifuentes.madero-1@ou.edu>
2021-12-26 23:55:20 -0600

    Created calculator layout

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Main 
+import java.awt.event.*;
+
+
+public class Main extends JFrame implements ActionListener
 {
 	public static void main(String[] args)
 	{
+		JFrame frame = new JFrame("Calculator");
+		
 		JButton zero = new JButton("0");
 		JButton one = new JButton("1");
 		JButton two = new JButton("2");
@@ -16,7 +23,47 @@ public class Main
 		JButton eight = new JButton("8");
 		JButton nine = new JButton("9");
 		
-		JLabel textField = new JLabel();
+		JButton add = new JButton("+");
+		JButton subtract = new JButton("-");
+		JButton divide = new JButton("/");
+		JButton multiply = new JButton("*");
+		JButton equals = new JButton("=");
+		JButton dot = new JButton(".");
+		
+		JButton clear = new JButton("Clear");
+		
+		JLabel textField = new JLabel("something");
+		
+		JPanel panel = new JPanel();
+		
+		panel.add(seven);
+		panel.add(eight);
+		panel.add(nine);
+		panel.add(add);
+		panel.add(four);
+		panel.add(five);
+		panel.add(six);
+		panel.add(subtract);
+		panel.add(one);
+		panel.add(two);
+		panel.add(three);
+		panel.add(multiply);
+		panel.add(zero);
+		panel.add(dot);
+		panel.add(clear);
+		panel.add(equals);
+		
+		frame.add(textField);
+		frame.add(panel);
+		frame.setSize(350,200);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
 	}
 }
