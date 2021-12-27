508cfa16f63da59993ae4a509d5e5cc31b0f498b
Diego Cifuentes <diego.o.cifuentes.madero-1@ou.edu>
2021-12-26 23:37:29 -0600

    Added JButtons

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		JButton zero = new JButton("0");
+		JButton one = new JButton("1");
+		JButton two = new JButton("2");
+		JButton three = new JButton("3");
+		JButton four = new JButton("4");
+		JButton five = new JButton("5");
+		JButton six = new JButton("6");
+		JButton seven = new JButton("7");
+		JButton eight = new JButton("8");
+		JButton nine = new JButton("9");
+		
+		JLabel textField = new JLabel();
+
+	}
+}
